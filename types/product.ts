9ec13bfd45feca1577cb56a39a11910e10ee0e61
Cauteros974 <pusik6666@ukr.net>
2026-07-05 10:24:05 +0300

    Update product.ts

--- types/product.ts
+++ types/product.ts
@@ -10,4 +10,11 @@ export type Category = {
     id: CategoryId;
     title: string;
     icon: string;
+};
+
+export type Product = {
+    id: string;
+    title: string;
+    description: string;
+    categoryId: string;
 }
