b7c9ce73ac3ff39e8dd9fc6be4c5901d9fa94438
Cauteros974 <pusik6666@ukr.net>
2026-07-08 20:09:12 +0300

    Update products.ts

--- data/products.ts
+++ data/products.ts
@@ -3,4 +3,5 @@ import { Category, Product } from "../types/product";
 export const categories: Category[] = [
     { id: "electronics", title: "Electronics", icon: "phone-portrait-outline" },
     { id: "fashion", title: "Fashion", icon: "shirt-outline" },
+    { id: "home", title: "Home", icon: "home-outline" },
 ]
