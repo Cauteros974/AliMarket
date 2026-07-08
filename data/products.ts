403bb764bde7e8075e7ea408c09c6dc8a19cc94f
Cauteros974 <pusik6666@ukr.net>
2026-07-08 20:09:00 +0300

    Create products.ts

---
+++ data/products.ts
@@ -0,0 +1,6 @@
+import { Category, Product } from "../types/product";
+
+export const categories: Category[] = [
+    { id: "electronics", title: "Electronics", icon: "phone-portrait-outline" },
+    { id: "fashion", title: "Fashion", icon: "shirt-outline" },
+]
