9b4634bd715f1958b50fcbd3764f78b19c0c1213
Cauteros974 <pusik6666@ukr.net>
2026-07-09 19:24:08 +0300

    Update useShopStore.ts

--- store/useShopStore.ts
+++ store/useShopStore.ts
@@ -2,6 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 import { CartItem } from "../types/product";
+import { products } from "../data/products";
 
 type ShopState = {
     cart: CartItem[];
@@ -9,6 +10,7 @@ type ShopState = {
     searchQuery: string;
     addToCart: (productId: string) => void;
     removeFromCart: (productId: string) => void;
+    increaseQuantity: (productId: string) => void;
 }
 
 export const useShopStore = create<ShopState>()(
@@ -36,7 +38,15 @@ export const useShopStore = create<ShopState>()(
         removeFromCart: (productId) => 
             set((state) => ({
                 cart: state.cart.filter((item) => item.productId !== productId),
-            }))
+            })),
+        increaseQuantity: (productId) =>
+            set((state) => ({
+                cart: state.cart.map((item) =>
+                    item.productId === productId
+                        ? { ...item, quantity: item.quantity + 1 }
+                        : item
+                ),
+            })),
         })
     )
 )
