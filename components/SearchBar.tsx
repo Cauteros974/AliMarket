5ef278ce0e0b162cd6dd01d1109a35b81ce3902a
Cauteros974 <pusik6666@ukr.net>
2026-07-10 19:06:10 +0300

    Update SearchBar.tsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -19,6 +19,7 @@ export default function SearchBar({
             <TextInput 
                 value={value}
                 onChangeText={onChangeText}
+                placeholder={placeholder}
             />
         </View>
     );
