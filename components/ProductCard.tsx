4244515e001237fb8dcdca11379e5f104501bba8
Cauteros974 <pusik6666@ukr.net>
2026-07-10 19:47:11 +0300

    Update ProductCard.tsx

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -5,21 +5,21 @@ import { Product } from "../types/product";
 import { formatPrice } from "../utils/format";
 
 type ProductCardProps = {
-
+    product: string
 };
 
 export default function ProductCard({
-
+    product
 }: ProductCardProps) {
     return(
         <Pressable onPress={onPress} style={styles.card}>
             <View style={styles.imageWrap}>
-
+                <Image source={{ uri: product.image }} style={styles.image} />}}
             </View>
         </Pressable>
     )
 };
 
 const styles = StyleSheet.create({
-    
+
 })
