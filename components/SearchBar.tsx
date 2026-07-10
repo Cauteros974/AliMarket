a9aacd878b8316cf14c703bcb8e6f56e53fc77a5
Cauteros974 <pusik6666@ukr.net>
2026-07-10 18:56:36 +0300

    Update SearchBar.tsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,14 @@
 import { Ionicons } from '@expo/vector-icons';
 import { StyleSheet, TextInput, View } from 'react-native';
 
-export default function SearchBar({
+type SearchBarProps = {
     value: string,
     onChangeText: (value: string) => void;
     placeholder?: string;
+}
+
+export default function SearchBar({
+    value,
+    onChangeText,
+    placeholder = "Search products",
 })
