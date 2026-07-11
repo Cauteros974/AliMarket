66a5537bffa48196c3e39b6ee10d6ea95c3159ea
Cauteros974 <pusik6666@ukr.net>
2026-07-11 18:50:50 +0300

    Create SectionHeader.tsx

---
+++ components/SectionHeader.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import { colors } from "../theme/colors";
+
+type SectionHeaderProps = {
+  title: string;
+  action?: string;
+};
+
+export default function SectionHeader({title, action}: SectionHeaderProps) {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}>{title}</Text>
+            {action ? <Text style={styles.action}>{action}</Text> : null}
+        </View>
+    )
+}
