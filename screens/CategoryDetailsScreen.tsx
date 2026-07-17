9da88c4e27598b1df0d0fa853b91f5330fb38ea9
Cauteros974 <pusik6666@ukr.net>
2026-07-17 20:32:54 +0300

    Update CategoryDetailsScreen.tsx

--- screens/CategoryDetailsScreen.tsx
+++ screens/CategoryDetailsScreen.tsx
@@ -1,5 +1,9 @@
 import { NativeAppEventEmitter } from "react-native";
+import { categories } from "../data/products";
+import { useShopStore } from "../store/useShopStore";
 
 export default function CategoryDetailsScreen({navigation, route}: Props) {
     const category = categories.find((item) => item.id === route.params.categoryId);
+    const favoriteIds = useShopStore((state) => state.favoriteIds);
+    const toggleFavorite = useShopStore((state) => state.toggleFavorite);
 }
