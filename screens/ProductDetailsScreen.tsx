b9ba544db9cac071b72de29b261acdfa0f6cf6bc
Cauteros974 <pusik6666@ukr.net>
2026-07-18 18:58:13 +0300

    Update ProductDetailsScreen.tsx

--- screens/ProductDetailsScreen.tsx
+++ screens/ProductDetailsScreen.tsx
@@ -70,9 +70,13 @@ export default function ProductDetailsScreen({route}: ProductDetailsProps) {
                     </View>
                 </View>
 
-                <Text>
-                    
-                </Text>
+                <Text style={styles.sectionTitle}>Colors</Text>
+
+                <View>
+                    {product.colors.map((color) => (
+                        <View key={color} style={[styles.colorDot, { backgroundColor: color }]} />
+                    ))}
+                </View>
             </ScrollView>
 
             <View>
