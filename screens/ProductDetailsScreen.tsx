b1a50abdb3611ffd2e2c3218590b77c695b33474
Cauteros974 <pusik6666@ukr.net>
2026-07-18 18:58:59 +0300

    Update ProductDetailsScreen.tsx

--- screens/ProductDetailsScreen.tsx
+++ screens/ProductDetailsScreen.tsx
@@ -77,6 +77,9 @@ export default function ProductDetailsScreen({route}: ProductDetailsProps) {
                         <View key={color} style={[styles.colorDot, { backgroundColor: color }]} />
                     ))}
                 </View>
+
+                <Text>Description</Text>
+                <Text>{product.description}</Text>
             </ScrollView>
 
             <View>
