67bdfc111e91bbb28c0a401915ab961ec50b04c4
Cauteros974 <pusik6666@ukr.net>
2026-07-18 18:37:24 +0300

    Update ProductDetailsScreen.tsx

--- screens/ProductDetailsScreen.tsx
+++ screens/ProductDetailsScreen.tsx
@@ -1,6 +1,7 @@
 import { Ionicons } from "@expo/vector-icons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Image } from "react-native";
+import { Image, View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { RootStackParamList } from "../navigation/types";
 import { products } from "../data/products";
 import { useShopStore } from "../store/useShopStore";
@@ -13,4 +14,14 @@ export default function ProductDetailsScreen({route}: ProductDetailsProps) {
     const addToCard = useShopStore((state) => state.addToCart);
     const favoriteIds = useShopStore((state) => state.favoriteIds);
     const toggleFavorite = useShopStore((state) => state.toggleFavorite);
+
+    if(!product) {
+        return(
+            <SafeAreaView>
+                <View>
+                    <Text style={styles.missingTitle}>Product not found</Text>
+                </View>
+            </SafeAreaView>
+        )
+    }
 }
