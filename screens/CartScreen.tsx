c6db4074a5eaca9c17fdeaee8b2c0943836f7ccb
Cauteros974 <pusik6666@ukr.net>
2026-07-18 19:52:56 +0300

    commit

---
+++ screens/CartScreen.tsx
@@ -0,0 +1,20 @@
+import { Ionicons } from "@expo/vector-icons";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Alert, FlatList, Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import QuantityStepper from "../components/QuantityStepper";
+import { products } from "../data/products";
+import { RootStackParamList } from "../navigation/types";
+import { useShopStore } from "../store/useShopStore";
+import { colors } from "../theme/colors";
+import { formatPrice } from "../utils/format";
+
+type CartScreenProps = {
+  navigation: NativeStackNavigationProp<RootStackParamList>;
+};
+
+export default function CartScreen({navigation}: CartScreenProps) {
+    const cart = useShopStore((state) => state.cart);
+    const increaseQuantity = useShopStore((state) => state.increaseQuantity);
+}
+
