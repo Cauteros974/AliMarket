69c77aca7e305068333a5c6820881c52193daeb9
Cauteros974 <pusik6666@ukr.net>
2026-07-18 18:34:10 +0300

    Update ProductDetailsScreen.tsx

--- screens/ProductDetailsScreen.tsx
+++ screens/ProductDetailsScreen.tsx
@@ -3,9 +3,12 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Image } from "react-native";
 import { RootStackParamList } from "../navigation/types";
 import { products } from "../data/products";
+import { useShopStore } from "../store/useShopStore";
 
 type ProductDetailsProps = NativeStackScreenProps<RootStackParamList, "ProductDetails">;
 
 export default function ProductDetailsScreen({route}: ProductDetailsProps) {
     const product = products.find((item) => item.id === route.params.productId);
+
+    const addToCard = useShopStore((state) => state.addToCart) 
 }
