51b6c6f5f3ee45a6c69d4429711d9221d3a37444
Cauteros974 <pusik6666@ukr.net>
2026-07-24 21:18:41 +0300

    Update ProfileScreen.tsx

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -5,5 +5,7 @@ import { useShopStore } from "../store/useShopStore";
 export default function ProfileScreen() {
     const cartCount = useShopStore((state) => 
         state.cart.reduce((sum,item) => sum + item.quantity, 0)
-    )
+    );
+
+    const favoriteCount = useShopStore((state) => state.favoriteIds.length);
 }
