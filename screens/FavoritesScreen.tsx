8b304ec971a0e112209754da6f3aadcf8a9faeb0
Cauteros974 <pusik6666@ukr.net>
2026-07-24 21:09:41 +0300

    Update FavoritesScreen.tsx

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -53,6 +53,7 @@ const styles = StyleSheet.create({
     title: {
         color: colors.text,
         fontSize: 28,
-        fontWeight: "800"
+        fontWeight: "900",
+        marginBottom: 16
     }
 })
