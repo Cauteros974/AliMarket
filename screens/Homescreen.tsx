7660aa145c63bf94f248a17f35925e3d5df37a77
Cauteros974 <pusik6666@ukr.net>
2026-07-25 20:37:59 +0300

    Update Homescreen.tsx

--- screens/Homescreen.tsx
+++ screens/Homescreen.tsx
@@ -121,7 +121,7 @@ const styles = StyleSheet.create({
     },
     title: {
         color: colors.text,
-        fontSize: 28,
+        fontSize: 26,
         fontWeight: "900",
         marginTop: 4,
     },
