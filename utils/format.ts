3b0596c0c4cf274d264d57e628979e5317d99101
Cauteros974 <pusik6666@ukr.net>
2026-07-25 21:39:19 +0300

    Update format.ts

--- utils/format.ts
+++ utils/format.ts
@@ -16,3 +16,9 @@ export function formatDate(value: string) {
         year: "numeric",
     }).format(new Date(value));
 }
+
+export function createId(prefix: string) {
+    return `${prefix}-${Date.now().toString(36)}-${Math.random()
+    .toString(36)
+    .slice(2, 7)}`;
+}
