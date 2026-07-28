8d7a106cbd8a0da500d70d6b9b728325c94e90c1
Cauteros974 <pusik6666@ukr.net>
2026-07-28 20:38:39 +0300

    Update CatalogScreen.tsx

--- screens/CatalogScreen.tsx
+++ screens/CatalogScreen.tsx
@@ -116,6 +116,8 @@ export default function CatalogScreen({ navigation }: CatalogScreenProps) {
                                 </Pressable>
                             ))}
                         </View>
+
+                        <Text style={styles.blockTitle}>Filters</Text>
                     </View>
                 } 
             />
