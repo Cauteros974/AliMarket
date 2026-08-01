c54925f8b2e3bf55857386f917eb9c9232e7fbf8
Cauteros974 <pusik6666@ukr.net>
2026-08-01 21:05:22 +0300

    Update CheckoutScreen.tsx

--- screens/CheckoutScreen.tsx
+++ screens/CheckoutScreen.tsx
@@ -153,5 +153,9 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "space-between",
         marginTop: 10,
+    },
+    value: {
+        color: colors.text,
+        fontWeight: "900",
     }
 })
