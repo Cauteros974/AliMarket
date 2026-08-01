0b1d689d61eed2aadc5741deed187f30512503fb
Cauteros974 <pusik6666@ukr.net>
2026-08-01 20:59:13 +0300

    Update CheckoutScreen.tsx

--- screens/CheckoutScreen.tsx
+++ screens/CheckoutScreen.tsx
@@ -126,5 +126,7 @@ const styles = StyleSheet.create({
         borderRadius: 18,
         borderWidth: 1,
         borderColor: colors.border,
+        padding: 16,
+        marginBottom: 12,
     }
 })
