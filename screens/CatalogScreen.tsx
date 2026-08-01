743b2b25b4ed03176bc795cfaed3a4a4e028fb66
Cauteros974 <pusik6666@ukr.net>
2026-08-01 19:38:56 +0300

    Update CatalogScreen.tsx

--- screens/CatalogScreen.tsx
+++ screens/CatalogScreen.tsx
@@ -1,18 +1,22 @@
-import { useMemo } from "react";
-import { FlatList, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useMemo, useState } from "react";
+import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CategoryChip from "../components/CategoryChip";
 import ProductCard from "../components/ProductCard";
 import SearchBar from "../components/SearchBar";
 import { categories, products } from "../data/products";
+import { RootStackParamList } from "../navigation/types";
 import { useShopStore } from "../store/useShopStore";
 import { colors } from "../theme/colors";
-import { SortOption } from "../types/shop";
+import { CategoryId } from "../types/product";  
 
 
 type CatalogScreenProps = {
-  navigation: any;
+    navigation: NativeStackNavigationProp<RootStackParamList>;
 };
+
+type FilterId = CategoryId | "all";
 
 const sortOptions: { label: string; value: SortOption }[] = [
   { label: "Popular", value: "popular" },
