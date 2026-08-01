b1354cc54a99b9dd5e8d629274487d2b476535bf
Cauteros974 <pusik6666@ukr.net>
2026-08-01 20:31:10 +0300

    Create CheckoutScreen.tsx

---
+++ screens/CheckoutScreen.tsx
@@ -0,0 +1,15 @@
+import { Alert, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { products } from "../data/products";
+import { useShopStore } from "../store/useShopStore";
+import { colors } from "../theme/colors";
+import { formatPrice } from "../utils/format";
+
+type Props = {
+    navigation: any;
+}
+
+export default function CheckoutScreen({navigation}: Props) {
+    const cart = useShopStore((state) => state.cart);
+    const addresses = useShopStore((state) => state.addresses);
+}
