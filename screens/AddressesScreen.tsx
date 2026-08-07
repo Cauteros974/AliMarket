33e155138ec8411e2799deb2db234b66337e44b3
Cauteros974 <pusik6666@ukr.net>
2026-08-07 21:37:52 +0300

    Update AddressesScreen.tsx

--- screens/AddressesScreen.tsx
+++ screens/AddressesScreen.tsx
@@ -160,7 +160,7 @@ const styles = StyleSheet.create({
         color: colors.text,
         fontWeight: "800",
     },
-    PrimaryButton: {
+    primaryButton: {
         height: 54,
         borderRadius: 18,
         backgroundColor: colors.primary,
