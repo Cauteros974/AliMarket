3872bebc3acf7081d3ee640525f408554bf8fe50
Cauteros974 <pusik6666@ukr.net>
2026-08-07 21:12:13 +0300

    Update AddressesScreen.tsx

--- screens/AddressesScreen.tsx
+++ screens/AddressesScreen.tsx
@@ -5,10 +5,21 @@ import { useShopStore } from "../store/useShopStore";
 import { colors } from "../theme/colors";
 
 export default function AddressesScreen() {
+    const addresses = useShopStore((state) => state.addresses);
+
+
     return(
         <SafeAreaView style={styles.safeArea}>
             <ScrollView contentContainerStyle={styles.content}>
                 <Text style={styles.title}>Addresses</Text>
+
+                {addresses.map((address) => (
+                    <Pressable>
+                        <View>
+                            <Text>{address.title}</Text>
+                        </View>
+                    </Pressable>
+                ))}
             </ScrollView>
         </SafeAreaView>
     )
