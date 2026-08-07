6e15eb079df8517c79e57bb4cb4014af344e81ef
Cauteros974 <pusik6666@ukr.net>
2026-08-07 21:20:17 +0300

    Update AddressesScreen.tsx

--- screens/AddressesScreen.tsx
+++ screens/AddressesScreen.tsx
@@ -17,6 +17,11 @@ export default function AddressesScreen() {
 
     function submit() {
         if(!city.trim() || !street.trim() || !phone.trim()) return;
+
+        addresses({
+            title: "New address",
+            city,
+        });
     }
 
 
