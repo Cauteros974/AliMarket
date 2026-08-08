b8c1cc8dde8c1c9bc2a4e4e2eec192f76ac3161a
Cauteros974 <pusik6666@ukr.net>
2026-08-08 20:22:53 +0300

    Create NotificationsScreen.tsx

---
+++ screens/NotificationsScreen.tsx
@@ -0,0 +1,21 @@
+import { Ionicons } from "@expo/vector-icons";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useShopStore } from "../store/useShopStore";
+import { colors } from "../theme/colors";
+import { formatDate } from "../utils/format";
+
+export default function NotificationsScreen() {
+    const notifications = useShopStore((state) => state.notifications);
+    const markNotificationRead = useShopStore((state) => state.markNotificationRead);
+
+    return(
+        <SafeAreaView>
+            <FlatList 
+                data={notifications}
+                keyExtractor={(item) => item.id}
+                contentContainerStyle={styles.content}
+            />
+        </SafeAreaView>
+    )
+}
