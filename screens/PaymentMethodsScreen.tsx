b289fe44180281f5826eb1280bfaf4da52c3645c
Cauteros974 <pusik6666@ukr.net>
2026-08-13 20:23:14 +0300

    Update PaymentMethodsScreen.tsx

--- screens/PaymentMethodsScreen.tsx
+++ screens/PaymentMethodsScreen.tsx
@@ -41,7 +41,7 @@ export default function PaymentMethodsScreen() {
 
         setCards((current) => [newCard, ...current]);
         setCardNumber("");
-        Alert.alert("Card added");
+        Alert.alert("Card added", "Demo payment card was saved.");
 
     }
 
