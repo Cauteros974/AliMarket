eb937b5f06352e38f0872752023c569b032fb0db
Cauteros974 <pusik6666@ukr.net>
2026-08-23 21:38:38 +0300

    Update AllFiltersModal.tsx

--- components/AllFiltersModal.tsx
+++ components/AllFiltersModal.tsx
@@ -47,7 +47,7 @@ export default function AllFiltersModal({visible, onClose}: AllFiltersModalProps
                     <Pressable
                         onPress={() => updateFilters({ freeDeliveryOnly: !filters.freeDeliveryOnly })}
                     >
-                        <Text style={[styles.optionText, filters.freeDeliveryOnly && styles.activeOptionText]}>
+                        <Text style={[styles.optionText]}>
                             Free delivery only
                         </Text>
                     </Pressable>
