27abd62d9f8ed05c30a3174587e388b9d47c3582
Cauteros974 <pusik6666@ukr.net>
2026-08-23 20:53:08 +0300

    Create SearchSuggestions.tsx

---
+++ components/SearchSuggestions.tsx
@@ -0,0 +1,17 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { products } from "../data/products";
+import { colors } from "../theme/colors";
+
+export default function SearchSuggestions(){
+    const suggestions = Array.from(
+        new(
+            products.filter((product) => product.title.toLowerCase().includes(normalized))
+        )
+    )
+
+    return(
+        <View>
+
+        </View>
+    )
+}
