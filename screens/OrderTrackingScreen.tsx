51cf2e25d73e63e15483258fb600e57a683c5978
Cauteros974 <pusik6666@ukr.net>
2026-08-24 15:22:24 +0300

    Update OrderTrackingScreen.tsx

--- screens/OrderTrackingScreen.tsx
+++ screens/OrderTrackingScreen.tsx
@@ -3,8 +3,15 @@ import { ScrollView, View, Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { RootStackParamList } from "../navigation/types";
 import { useShopStore } from "../store/useShopStore";
+import { OrderStatus } from "../types/shop";
 
 type Props = NativeStackScreenProps<RootStackParamList, "OrderTracking">;
+
+const steps: OrderStatus[] = [
+    "Processing",
+    "Shipped",
+    "Delivered",
+]
 
 export default function OrderTrackingScreen({ route }: Props) {
     const order = useShopStore((state) => 
