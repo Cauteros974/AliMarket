7fef594bdb4a62d4a5dd1824f2edbbdb7572c822
Cauteros974 <pusik6666@ukr.net>
2026-08-24 16:38:07 +0300

    Update SellerScreen.tsx

--- screens/SellerScreen.tsx
+++ screens/SellerScreen.tsx
@@ -81,5 +81,7 @@ const styles = StyleSheet.create({
         borderRadius: 22,
         borderWidth: 1,
         borderColor: colors.border,
+        padding: 16,
+        marginBottom: 16,
     },
 })
