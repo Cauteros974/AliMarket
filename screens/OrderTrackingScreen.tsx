b21690cb1d8d0d3e2469a9a4148616497db672ee
Cauteros974 <pusik6666@ukr.net>
2026-08-24 15:15:00 +0300

    Create OrderTrackingScreen.tsx

---
+++ screens/OrderTrackingScreen.tsx
@@ -0,0 +1,15 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { ScrollView, View, Text, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { RootStackParamList } from "../navigation/types";
+import { useShopStore } from "../store/useShopStore";
+
+export default function OrderTrackingScreen(){
+    return(
+        <SafeAreaView>
+            <SafeAreaView>
+                <Text>Tracking</Text>
+            </SafeAreaView>
+        </SafeAreaView>
+    )
+}
