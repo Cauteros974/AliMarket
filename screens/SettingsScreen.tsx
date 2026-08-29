cb399d15f9c6dd7b096ebc141fddb6df10d81e60
Cauteros974 <pusik6666@ukr.net>
2026-08-29 21:31:34 +0300

    Update SettingsScreen.tsx

---
+++ screens/SettingsScreen.tsx
@@ -0,0 +1,16 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useShopStore } from "../store/useShopStore";
+import { colors } from "../theme/colors";
+import { AppTheme, Locale } from "../types/shop";
+
+export default function SettingsScreen() {
+    return(
+        <SafeAreaView>
+            <View>
+                 <Text>Settings</Text>
+                 <Text> Theme and language are saved locally.</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
