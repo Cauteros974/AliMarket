00feb72a15351dddf87e0c743d4c926fd35cc7a0
Cauteros974 <pusik6666@ukr.net>
2026-09-03 15:14:57 +0300

    Update QRScannerScreen.tsx

--- screens/QRScannerScreen.tsx
+++ screens/QRScannerScreen.tsx
@@ -112,6 +112,8 @@ const styles = StyleSheet.create({
         borderRadius: 18,
         backgroundColor: colors.primary,
         paddingHorizontal: 18,
-        alignItems: "center"
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 12,
     }
 })
