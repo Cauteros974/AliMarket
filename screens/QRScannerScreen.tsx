6b69f1b3ea557cea73274e77ab0f8332c0d6b916
Cauteros974 <pusik6666@ukr.net>
2026-09-03 14:56:38 +0300

    Update QRScannerScreen.tsx

--- screens/QRScannerScreen.tsx
+++ screens/QRScannerScreen.tsx
@@ -35,6 +35,10 @@ export default function QRScannerScreen(){
         <SafeAreaView>
             <View>
                 <Text>QR scanner</Text>
+
+                <Text>
+                    Try scanning a code with WELCOME10, SALE15 or FREESHIP.
+                </Text>
             </View>
         </SafeAreaView>
     )
