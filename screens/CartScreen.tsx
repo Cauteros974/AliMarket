1a7032f279de498d00cf31a6c6d61ff288f5ac93
Cauteros974 <pusik6666@ukr.net>
2026-09-04 20:55:47 +0300

    Update CartScreen.tsx

--- screens/CartScreen.tsx
+++ screens/CartScreen.tsx
@@ -69,7 +69,7 @@ export default function CartScreen({ navigation }: Props) {
                 onPress={() => navigation.navigate("ProductDetails", { productId: item.product!.id })}
                 style={styles.cartItem}
               >
-                <Image source={{ uri: item.product.image }} style={styles.image} />
+                <Image source={item.product.image} style={styles.image} />
 
                 <View style={styles.itemInfo}>
                   <Text numberOfLines={2} style={styles.itemTitle}>
