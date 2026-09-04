8a82bc4ce7f78e8fc4a0fe9cf1ce6005297e229d
Cauteros974 <pusik6666@ukr.net>
2026-09-04 20:39:39 +0300

    Update useShopStore.ts

--- store/useShopStore.ts
+++ store/useShopStore.ts
@@ -353,9 +353,9 @@ export const useShopStore = create<ShopState>()(
         addRecentlyViewed: (productId) => 
           set((state) => {
             const filtered = state.recentlyViewedIds.filter((id) => id !== productId);
-            return(
+            return{
               recentlyViewedIds: [productId, ...filtered].slice(0, 10),
-            )
+            }
           })
     }),
     
