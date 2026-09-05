5567ba2851cb7aabfc091210de414e9fe6fcc704
Cauteros974 <pusik6666@ukr.net>
2026-09-05 19:55:39 +0300

    Create StackBackButton.tsx

---
+++ components/StackBackButton.tsx
@@ -0,0 +1,14 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Pressable, StyleSheet } from "react-native";
+import { colors } from "../theme/colors";
+
+export default function StackBackButton({}){
+    return(
+        <Pressable
+            style={styles.button}
+            hitSlop={10}
+        >
+
+        </Pressable>
+    )
+}
