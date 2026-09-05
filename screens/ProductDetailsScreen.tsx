15416304081ad6de62e534f4c892084020ce7cf8
Cauteros974 <pusik6666@ukr.net>
2026-09-05 19:27:31 +0300

    Update ProductDetailsScreen.tsx

--- screens/ProductDetailsScreen.tsx
+++ screens/ProductDetailsScreen.tsx
@@ -133,7 +133,7 @@ const styles = StyleSheet.create({
 
     width: 44,
     height: 44,
-    
+    borderRadius: 20,
   },
   image: { width: "100%", height: "100%" },
   info: { paddingHorizontal: 18 },
