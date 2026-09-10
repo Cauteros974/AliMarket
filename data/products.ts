643ef6fbb28fe4bc13c368b59df19aaf4bbeb55f
Cauteros974 <pusik6666@ukr.net>
2026-09-10 20:23:04 +0300

    Update products.ts

--- data/products.ts
+++ data/products.ts
@@ -328,7 +328,7 @@ export const products: Product[] = [
         sold: 160,
         discountLabel: "Hot",
         delivery: "Free delivery in 7-12 days",
-        colors: ["#0be7c9", "#089449", "#F97316", "#030303", "#f92d16"],
+        colors: ["#0d1be7", "#089449", "#F97316", "#030303", "#f91616"],
     },
 ];
 
