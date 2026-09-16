52be59e1efc5b4fc8da3620aadaa99bb418b5cd6
Cauteros974 <pusik6666@ukr.net>
2026-09-16 20:40:41 +0300

    Update NotificationsScreen.tsx

--- screens/NotificationsScreen.tsx
+++ screens/NotificationsScreen.tsx
@@ -12,6 +12,7 @@ export default function NotificationsScreen() {
 
     async function sendDemoPush() {
         const permission = await Notifications.requestPermissionsAsync();
+        if(!permission) return;
     }
 
     return(
