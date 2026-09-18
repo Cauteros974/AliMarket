b03715c79f0b9304ef8f4d435c60f4b784794ea1
Cauteros974 <pusik6666@ukr.net>
2026-09-18 20:14:09 +0300

    Update useShopStore.ts

--- store/useShopStore.ts
+++ store/useShopStore.ts
@@ -231,7 +231,7 @@ export const useShopStore = create<ShopState>()(
       register: (name, email) =>
         set({ user: { id: createId("user"), name, email } }),
 
-      login: (email, name = "Alimarket Customer") =>
+      login: (email) =>
         set({
           user: {
             id: createId("user"),
