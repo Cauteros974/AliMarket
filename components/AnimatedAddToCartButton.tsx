86b07ecae0ca3ae04b25a9b8c191e96c287d97ce
Cauteros974 <pusik6666@ukr.net>
2026-09-26 20:48:00 +0300

    Update AnimatedAddToCartButton.tsx

--- components/AnimatedAddToCartButton.tsx
+++ components/AnimatedAddToCartButton.tsx
@@ -61,6 +61,6 @@ export default function AnimatedAddToCartButton({
             setTimeout(() => {
                 setAdded(false);
             }, 1200);
-        }, 300)
+        }, 350)
       }
 }
